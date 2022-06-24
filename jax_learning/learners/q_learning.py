5ed62270904fdbba47af4cccddef0019aaaf4a9c
chanb <bryanchan13@gmail.com>
2022-06-24 15:29:04 +0800

    maybe more self contained

--- jax_learning/learners/q_learning.py
+++ jax_learning/learners/q_learning.py
@@ -3,13 +3,14 @@ from typing import Tuple
 
 import equinox as eqx
 import jax
+import jax.numpy as jnp
 import numpy as np
 import optax
 
 from jax_learning.buffers.buffers import ReplayBuffer
 from jax_learning.buffers.utils import to_jnp, batch_flatten
 from jax_learning.learners.learners import Learner
-from jax_learning.losses.value_loss import q_learning_loss
+from jax_learning.losses.value_loss import one_step_bellman_optimality_error
 
 MEAN_LOSS = "mean_loss"
 MEAN_Q_CURR = "mean_q_curr"
@@ -41,6 +42,34 @@ class QLearning(Learner):
         self._update_frequency = cfg.update_frequency
         self._target_update_frequency = cfg.target_update_frequency
         self._omega = cfg.omega
+
+        @eqx.filter_grad(has_aux=True)
+        def q_learning_loss(models: Tuple[eqx.Module, eqx.Module],
+                            obss: np.ndarray,
+                            h_states: np.ndarray,
+                            acts: np.ndarray,
+                            rews: np.ndarray,
+                            dones: np.ndarray,
+                            next_obss: np.ndarray,
+                            next_h_states: np.ndarray,
+                            gammas: np.ndarray) -> Tuple[np.ndarray, dict]:
+            (model, target_model) = models
+            q_curr_preds, _ = jax.vmap(model.q_values)(obss, h_states)
+            q_next_preds, _ = jax.vmap(target_model.q_values)(next_obss, next_h_states)
+            
+            td_errors = jax.vmap(one_step_bellman_optimality_error)(q_curr_preds, acts, q_next_preds, rews, dones, gammas)
+            loss = jnp.mean(td_errors ** 2)
+            return loss, {
+                "loss": loss,
+                "max_q_next": jnp.max(q_next_preds),
+                "min_q_next": jnp.min(q_next_preds),
+                "mean_q_next": jnp.mean(q_next_preds),
+                "max_q_curr": jnp.max(q_curr_preds),
+                "min_q_curr": jnp.min(q_curr_preds),
+                "mean_q_curr": jnp.mean(q_curr_preds),
+                "max_td_error": jnp.max(td_errors),
+                "min_td_error": jnp.min(td_errors),
+            }
         
         def step(model: eqx.Module,
                  target_model: eqx.Module,
--- jax_learning/losses/value_loss.py
+++ jax_learning/losses/value_loss.py
@@ -1,7 +1,3 @@
-from typing import Tuple
-
-import equinox as eqx
-import jax
 import jax.numpy as jnp
 import numpy as np
 
@@ -14,31 +10,3 @@ def one_step_bellman_optimality_error(q_curr_pred: np.ndarray,
                                       gamma: np.ndarray) -> np.ndarray:
     q_target = rew + (1 - done) * (gamma * jnp.max(q_next_pred))
     return q_curr_pred[act] - q_target
-
-@eqx.filter_grad(has_aux=True)
-def q_learning_loss(models: Tuple[eqx.Module, eqx.Module],
-                    obss: np.ndarray,
-                    h_states: np.ndarray,
-                    acts: np.ndarray,
-                    rews: np.ndarray,
-                    dones: np.ndarray,
-                    next_obss: np.ndarray,
-                    next_h_states: np.ndarray,
-                    gammas: np.ndarray) -> Tuple[np.ndarray, dict]:
-    (model, target_model) = models
-    q_curr_preds, _ = jax.vmap(model.q_values)(obss, h_states)
-    q_next_preds, _ = jax.vmap(target_model.q_values)(next_obss, next_h_states)
-    
-    td_errors = jax.vmap(one_step_bellman_optimality_error)(q_curr_preds, acts, q_next_preds, rews, dones, gammas)
-    loss = jnp.mean(td_errors ** 2)
-    return loss, {
-        "loss": loss,
-        "max_q_next": jnp.max(q_next_preds),
-        "min_q_next": jnp.min(q_next_preds),
-        "mean_q_next": jnp.mean(q_next_preds),
-        "max_q_curr": jnp.max(q_curr_preds),
-        "min_q_curr": jnp.min(q_curr_preds),
-        "mean_q_curr": jnp.mean(q_curr_preds),
-        "max_td_error": jnp.max(td_errors),
-        "min_td_error": jnp.min(td_errors),
-    }
