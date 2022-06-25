af0becae0c43e8165a9247e4077abe1c86afc0e5
chanb <bryanchan13@gmail.com>
2022-06-25 16:23:57 +0800

    REINFORCE test

--- jax_learning/agents/rl_agents.py
+++ jax_learning/agents/rl_agents.py
@@ -9,7 +9,37 @@ from jax_learning.buffers import ReplayBuffer
 from jax_learning.learners import Learner
 
 
-class EpsilonGreedyAgent(LearningAgent):
+class RLAgent(LearningAgent):
+    def __init__(self,
+                 model: eqx.Module,
+                 buffer: ReplayBuffer,
+                 learner: Learner,
+                 key: jrandom.PRNGKey):
+        super().__init__(model, buffer, learner)
+        self._key = key
+        
+    def deterministic_action(self,
+                             obs: np.ndarray,
+                             h_state: np.ndarray,
+                             info: dict) -> Tuple[np.ndarray ,np.ndarray]:
+        action, next_h_state = self.model.deterministic_action(obs, h_state)
+        return np.asarray(action), np.asarray(next_h_state)
+
+    def compute_action(self,
+                       obs: np.ndarray,
+                       h_state: np.ndarray,
+                       info: dict,
+                       overwrite_rng_key: bool=True) -> Tuple[np.ndarray ,np.ndarray]:
+        new_key, curr_key = jrandom.split(self._key)
+        action, next_h_state = self.model.random_action(obs, h_state, curr_key)
+
+        if overwrite_rng_key:
+            self._key = new_key
+
+        return np.asarray(action), np.asarray(next_h_state)
+
+
+class EpsilonGreedyAgent(RLAgent):
     def __init__(self,
                  model: eqx.Module,
                  buffer: ReplayBuffer,
@@ -19,26 +49,18 @@ class EpsilonGreedyAgent(LearningAgent):
                  eps_decay: float,
                  eps_warmup: float,
                  key: jrandom.PRNGKey):
-        super().__init__(model, buffer, learner)
+        super().__init__(model, buffer, learner, key)
         self._eps = init_eps
         self._init_eps = init_eps
         self._min_eps = min_eps
         self._eps_decay = eps_decay
         self._eps_warmup = eps_warmup
-        self._key = key
-        
-    def deterministic_action(self,
-                             obs: np.ndarray,
-                             h_state: np.ndarray,
-                             info: dict) -> Tuple[np.ndarray ,np.ndarray]:
-        action, next_h_state = self.model.deterministic_action(obs, h_state)
-        return np.asarray(action), np.asarray(next_h_state)
         
     def compute_action(self,
                        obs: np.ndarray,
-                    h_state: np.ndarray,
-                    info: dict,
-                    overwrite_rng_key: bool=True) -> Tuple[np.ndarray ,np.ndarray]:
+                       h_state: np.ndarray,
+                       info: dict,
+                       overwrite_rng_key: bool=True) -> Tuple[np.ndarray ,np.ndarray]:
         new_key, curr_key = jrandom.split(self._key)
         if jrandom.bernoulli(key=curr_key, p=self._eps):
             val, next_h_state = self.model.q_values(obs, h_state)
