df78f522ad52f936e7e399805a7c44d0b2cd7fd7
Bryan Chan <bryanchan13@gmail.com>
2022-09-09 05:24:45 -0600

    conflict

--- jax_learning/learners/path_consistency.py
+++ jax_learning/learners/path_consistency.py
@@ -104,10 +104,7 @@ class PCL(ReinforcementLearner):
             h_states: np.ndarray,
             acts: np.ndarray,
             rews: np.ndarray,
-<<<<<<< HEAD
             terminateds: np.ndarray,
-=======
->>>>>>> main
             lengths: np.ndarray,
         ) -> Tuple[
             StochasticPolicy,
